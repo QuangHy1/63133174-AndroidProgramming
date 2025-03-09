ec005e5df1f631c5da2ff6f5c8ba4074aee72efa
QuangHy1 <150933991+QuangHy1@users.noreply.github.com>
2025-03-09 22:02:26 +0700

    EX7

---
+++ Ex7_IntentLogin/app/src/main/java/com/example/ex7_intentlogin/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.ex7_intentlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
