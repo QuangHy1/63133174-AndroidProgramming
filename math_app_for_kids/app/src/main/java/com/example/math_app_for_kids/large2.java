52ee06ab6cb837fd4d2c7de0660c8cc2b8e31fc5
QuangHy1 <150933991+QuangHy1@users.noreply.github.com>
2025-03-23 18:00:08 +0700

    baitap2
    finish

---
+++ math_app_for_kids/app/src/main/java/com/example/math_app_for_kids/large2.java
@@ -0,0 +1,14 @@
+package com.example.math_app_for_kids;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class large2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_large2);
+    }
+}
