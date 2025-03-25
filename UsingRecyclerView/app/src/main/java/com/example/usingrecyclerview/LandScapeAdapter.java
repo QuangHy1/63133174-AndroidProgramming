afc7b7af1dd99a4ebb1a5f4abeaf2b547b7b00ee
QuangHy1 <150933991+QuangHy1@users.noreply.github.com>
2025-03-25 16:13:53 +0700

    recycleview

---
+++ UsingRecyclerView/app/src/main/java/com/example/usingrecyclerview/LandScapeAdapter.java
@@ -0,0 +1,52 @@
+package com.example.usingrecyclerview;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class LandScapeAdapter extends RecyclerView.Adapter<LandScapeAdapter.ItemLandHolder >
+{
+    Context context;
+    ArrayList <LandScape> lstData;
+
+    public LandScapeAdapter(Context context, ArrayList<LandScape> lstData) {
+        this.context = context;
+        this.lstData = lstData;
+    }
+
+    @NonNull
+    @Override
+    public ItemLandHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemLandHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class ItemLandHolder extends RecyclerView.ViewHolder{
+        TextView tvCaption;
+        ImageView imageViewLandscape;
+
+        public ItemLandHolder(@NonNull View itemView) {
+            super(itemView);
+            tvCaption = itemView.findViewById(R.id.textViewCation);
+            imageViewLandscape = itemView.findViewById(R.id.imageViewLand);
+        }
+
+
+    }
+}
