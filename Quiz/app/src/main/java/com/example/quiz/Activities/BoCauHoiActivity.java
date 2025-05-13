1387aa2d36e713151c8ef71bf53fbaddc7871639
QuangHy1 <150933991+QuangHy1@users.noreply.github.com>
2025-05-13 19:32:44 +0700

    up

---
+++ Quiz/app/src/main/java/com/example/quiz/Activities/BoCauHoiActivity.java
@@ -0,0 +1,24 @@
+package com.example.quiz.Activities;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.quiz.R;
+
+public class BoCauHoiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bo_cau_hoi);
+
+        ActionBar actionBar = getSupportActionBar(); // Lấy đối tượng ActionBar
+        if (actionBar != null) {
+            actionBar.hide(); // Chỉ ẩn nếu ActionBar tồn tại
+        }
+
+
+    }
+}
--- Quiz/app/src/main/java/com/example/quiz/MainActivity.java
+++ Quiz/app/src/main/java/com/example/quiz/MainActivity.java
@@ -2,11 +2,17 @@ package com.example.quiz;
 
 import androidx.appcompat.app.ActionBar;
 import  androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.quiz.Activities.BoCauHoiActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    CardView lichsu, khoahoc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +22,17 @@ public class MainActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.hide(); // Chỉ ẩn nếu ActionBar tồn tại
         }
+
+        lichsu = findViewById(R.id.lichsu);
+        khoahoc = findViewById(R.id.khoahoc);
+
+        lichsu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BoCauHoiActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
