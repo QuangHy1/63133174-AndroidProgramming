dfcedda072bf29f4f1e5e550b28c745cd083e2eb
QuangHy1 <150933991+QuangHy1@users.noreply.github.com>
2025-05-13 17:10:21 +0700

    up

--- Quiz/app/src/main/java/com/example/quiz/Activities/ChaoMungActivity.java
+++ Quiz/app/src/main/java/com/example/quiz/Activities/ChaoMungActivity.java
@@ -2,8 +2,11 @@ package com.example.quiz.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
+import com.example.quiz.MainActivity;
 import com.example.quiz.R;
 
 public class ChaoMungActivity extends AppCompatActivity {
@@ -12,5 +15,17 @@ public class ChaoMungActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chao_mung);
+
+        getSupportActionBar().hide(); // sử dụng để ẩn thanh ActionBar
+
+        // tạo đối tượng Handler
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(ChaoMungActivity.this, MainActivity.class); // chuyển từ ChaoMungActivity sang MainActivity
+                startActivity(intent);
+            }
+        },2000);// hiện layout activity_chao_mung.xml trong 2s, sau đó sang MainActivity
+
     }
 }
