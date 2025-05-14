55959914b4d2721f5411b81b9d4cb930cc3290fb
QuangHy1 <150933991+QuangHy1@users.noreply.github.com>
2025-05-14 11:22:48 +0700

    up

---
+++ Quiz/app/src/main/java/com/example/quiz/Activities/CauHoiActivity.java
@@ -0,0 +1,16 @@
+package com.example.quiz.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.quiz.R;
+
+public class CauHoiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cau_hoi);
+    }
+}
