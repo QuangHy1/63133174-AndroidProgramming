be426dff35554ede62ee89b03da06dbfa245506a
QuangHy1 <150933991+QuangHy1@users.noreply.github.com>
2025-05-14 11:06:28 +0700

    up

--- Quiz/app/src/main/java/com/example/quiz/Activities/BoCauHoiActivity.java
+++ Quiz/app/src/main/java/com/example/quiz/Activities/BoCauHoiActivity.java
@@ -2,23 +2,51 @@ package com.example.quiz.Activities;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
+import com.example.quiz.Adapters.bocauhoiAdapter;
+import com.example.quiz.Models.bocauhoiModel;
 import com.example.quiz.R;
+import com.example.quiz.databinding.ActivityBoCauHoiBinding;
+
+import java.util.ArrayList;
 
 public class BoCauHoiActivity extends AppCompatActivity {
 
-    
+    ActivityBoCauHoiBinding binding;
+    ArrayList<bocauhoiModel> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bo_cau_hoi);
+        binding = ActivityBoCauHoiBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         ActionBar actionBar = getSupportActionBar(); // Lấy đối tượng ActionBar
         if (actionBar != null) {
             actionBar.hide(); // Chỉ ẩn nếu ActionBar tồn tại
         }
+
+        list = new ArrayList<>();
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        binding.bocauhoiRecy.setLayoutManager(linearLayoutManager);
+
+        list.add(new bocauhoiModel("BỘ-1"));
+        list.add(new bocauhoiModel("BỘ-2"));
+        list.add(new bocauhoiModel("BỘ-3"));
+        list.add(new bocauhoiModel("BỘ-4"));
+        list.add(new bocauhoiModel("BỘ-5"));
+        list.add(new bocauhoiModel("BỘ-6"));
+        list.add(new bocauhoiModel("BỘ-7"));
+        list.add(new bocauhoiModel("BỘ-8"));
+        list.add(new bocauhoiModel("BỘ-9"));
+        list.add(new bocauhoiModel("BỘ-10"));
+
+        bocauhoiAdapter adapter = new bocauhoiAdapter(this,list);
+
+        binding.bocauhoiRecy.setAdapter(adapter);
 
 
     }
